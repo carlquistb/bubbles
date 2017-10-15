5858849127bb8f67828670ed109dcd0765001289
Brendan A Carlquist <carlquistb@gmail.com>
2017-10-15 12:08:38 -0700

    continue of last commit.

--- search.php
+++ search.php
@@ -16,16 +16,53 @@ include("common.php");
     </head>
     <body>
         <?php insertCommonHeader(); ?>
-        <p class="well">
+        <!-- <p class="well">
             Plans for this page:
             the goal of this page is to put in information about something, and be able to get the rest of the information about it.
             <br><br>Very simply, this page will provide a form for each kind of thing you can look up, and will direct you to "GET" pages that return tables of data.
             <br> using "GET" pages will mean that the URL can be copied and sent, and you can bookmark results and all that.
             <br><br> This will allow people to either get information they need, or get the ID of what they need to change.
-        </p>
+        </p>  -->
 
         <div class="row">
+            <div class="col-sm-12">
+                <div class="well well-lg">
+                    <!-- <form class="form-horizontal" action="searchUser.php" role="form" method="get">
+                        <fieldset>
 
+                        </fieldset>
+                    </form> -->
+                    <h2>search by first initial</h2>
+                    <?php $AZ = range('A','Z'); ?>
+                    <?php
+                        foreach($AZ as $letter) {
+                            ?> <a href="searchUser.php?searhterm=firstname&letter=<?php echo $letter; ?>"><?php echo $letter; ?> </a>
+                    <?php } ?>
+                </div>
+                <div class="well well-lg">
+                    <h2>search by last initial</h2>
+                    <?php $AZ = range('A','Z'); ?>
+                    <?php
+                    foreach($AZ as $letter) {
+                        ?> <a href="searchUser.php?searhterm=lastname&letter=<?php echo $letter; ?>"><?php echo $letter; ?> </a>
+                    <?php } ?>
+                </div>
+                <div class="well well-lg">
+                    <form class="form-horizontal" action="searchKey.php"></form>
+                    <legend>search by key serial</legend>
+                    <fieldset>
+                        <?php insertLabelTag('keyTypeID-select','key type');?>
+                        <select class="form-control" name="keyTypeID" id="keyTypeID-select">
+                            <?php fillKeyTypeSelectList(); ?>
+                        </select>
+                        <?php insertLabelTag('keyID-select','key serial');?>
+                        <select class="form-control" name="keyID" id="keyID-select">
+
+                        </select>
+                    </fieldset>
+
+                </div>
+            </div>
         </div>
     </body>
 </html>
--- searchUser.php
+++ searchUser.php
@@ -5,3 +5,29 @@
  * Date: 10/14/2017
  * Time: 12:52 AM
  */
+include("common.php");
+
+?>
+
+<html>
+    <head>
+        <?php insertCommonHead(); ?>
+    </head>
+    <body>
+        <?php
+            insertCommonHeader();
+
+            if($_GET['searchterm'] == 'firstname') {
+                $letter = $_GET['letter'];
+                selectToTable("select UserFirstName, UserLastName from Users where UserFirstName like '$letter%'");
+            }
+            if($_GET['searchterm'] == 'lastname') {
+                $letter = $_GET['letter'];
+                selectToTable("select UserFirstName, UserLastName from Users where UserLastName like '$letter%'");
+            }
+            if($_GET['searchterm'] == 'keyID') {
+                /* search all rentals ever to have that keyID, display in chronological order. */
+            }
+        ?>
+    </body>
+</html>
